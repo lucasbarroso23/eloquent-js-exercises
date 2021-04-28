de28b783ac21a4bb38c59dccabe03a6a869ef327
Lucas Barroso <lucasbarroso23@gmail.com>
2021-04-28 10:02:42 -0300

    exerc 2

--- cap2.js
+++ cap2.js
@@ -10,7 +10,7 @@ Write a loop that makes seven calls to console.log to output the following trian
 ####### */
 
 let count = 0;
-
+console.log('================= QUESTÃO 1 =====================');
 while (count < 8) {
   console.log('*'.repeat(count));
   count++;
@@ -20,6 +20,23 @@ while (count < 8) {
 Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
 
 When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those). */
+
+let count2 = 1;
+console.log('================= QUESTÃO 2 =====================');
+
+while (count2 <= 100) {
+  if (count2 % 3 === 0 && count2 % 5 === 0) {
+    console.log('BuzzFizz');
+  } else if (count2 % 5 === 0) {
+    console.log('Buzz');
+  } else if (count2 % 3 === 0) {
+    console.log('Fizz');
+  } else {
+    console.log(count2);
+  }
+
+  count2++;
+}
 
 /* 3 - Chessboard
 Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
