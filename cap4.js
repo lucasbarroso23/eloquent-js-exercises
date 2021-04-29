f7c83e25741d5fd0112927c21eb2e5c296d57143
lucasbarroso23 <lucasbarroso23@gmail.com>
2021-04-29 00:19:58 -0300

    cap4 exerc 1 & 2

--- cap4.js
+++ cap4.js
@@ -7,8 +7,34 @@ Write a range function that takes two arguments, start and end, and returns an a
 
 Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
 
-As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, 
+the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. 
+Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
 */
+
+console.log("================= QUESTÃO 1 =====================");
+
+function range(a, b, step = a < b ? 1 : -1) {
+  const array = [];
+  
+  if(step > 0) {
+    for(let ind = a; ind <= b; ind += step) {
+      array.push(ind);
+    }
+  } else {
+    for(let ind = a; ind >= b; ind += step) {
+      array.push(ind);
+    }
+  }
+  return array
+}
+
+
+function sum(array) {
+  return array.reduce((accum, curr) => accum + curr);
+}
+
+console.log(sum(range(5, 2, -1)));
 
 /*
 2 - Reversing an array
@@ -20,6 +46,28 @@ does what the reverse method does: it modifies the array given as argument by re
 Thinking back to the notes about side effects and pure functions in the previous chapter, 
 which variant do you expect to be useful in more situations? Which one runs faster?
  */
+
+console.log('================= QUESTÃO 2 =====================');
+function reverseArray(array) {
+  const newArray = [];
+  for(let a = array.length - 1; a >= 0; a--) {
+    newArray.push(array[a]);
+  }
+  return newArray;
+}
+
+console.log(reverseArray(['1', '2', '3', '4', '5']));
+
+function reverseArrayInPlace(array) {
+  for(let a = 0 ; a <= ~~(array.length/2); a++) {
+    let save = array[a];
+    array[a] = array[array.length -1 - a];
+    array[array.length -1 - a] = save;    
+  }
+  return array  
+}
+
+console.log(reverseArrayInPlace(['1', '2', '3', '4', '5', '6']));
 
 /*
 3 - A list
