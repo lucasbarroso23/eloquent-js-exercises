ffd40bd39573aaffaa20cd02487dcc0c43e65937
Lucas Barroso <lucasbarroso23@gmail.com>
2021-05-01 11:21:35 -0300

    cap5 exerc 4

--- cap5.js
+++ cap5.js
@@ -83,3 +83,80 @@ Remember that each script object has a direction property that can be "ltr" (lef
 The dominant direction is the direction of a majority of the characters that have a script associated with them. 
 The characterScript and countBy functions defined earlier in the chapter are probably useful here. 
 */
+var SCRIPTS = [
+  {
+    name: "Adlam",
+    ranges: [
+      [125184, 125259],
+      [125264, 125274],
+      [125278, 125280],
+    ],
+    direction: "rtl",
+    year: 1987,
+    living: true,
+    link: "https://en.wikipedia.org/wiki/Fula_alphabets#Adlam_alphabet",
+  },
+  {
+    name: "Caucasian Albanian",
+    ranges: [
+      [66864, 66916],
+      [66927, 66928],
+    ],
+    direction: "ltr",
+    year: 420,
+    living: false,
+    link: "https://en.wikipedia.org/wiki/Caucasian_Albanian_alphabet",
+  },
+  {
+    name: "Ahom",
+    ranges: [
+      [71424, 71450],
+      [71453, 71468],
+      [71472, 71488],
+    ],
+    direction: "ltr",
+    year: 1250,
+    living: false,
+    link: "https://en.wikipedia.org/wiki/Ahom_alphabet",
+  },
+];
+
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (
+      script.ranges.some(([from, to]) => {
+        return code >= from && code < to;
+      })
+    ) {
+      return script;
+    }
+  }
+  return null;
+}
+
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex((c) => c.name == name);
+    if (known == -1) {
+      counts.push({ name, count: 1 });
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+
+function dominantDirection(text) {
+  let counted = countBy(text, (char) => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter(({ name }) => name != "none");
+
+  if (counted.length == 0) return "ltr";
+
+  return counted.reduce((a, b) => (a.count > b.count ? a : b)).name;
+}
+
+console.log(dominantDirection("Hello!"));
